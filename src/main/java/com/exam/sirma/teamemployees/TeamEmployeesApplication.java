aa6e1b3d9539e3d4f6d6e17cec02522271a64cad
GorgiBg <georgid@orbisds.com>
2023-12-16 21:09:38 +0200

    add postgres config in application.yaml

--- src/main/java/com/exam/sirma/teamemployees/TeamEmployeesApplication.java
+++ src/main/java/com/exam/sirma/teamemployees/TeamEmployeesApplication.java
@@ -1,13 +1,17 @@
 package com.exam.sirma.teamemployees;
 
+import com.exam.sirma.teamemployees.utils.CSVReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeamEmployeesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamEmployeesApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+
+        CSVReader.read("src/main/resources/datafile.csv");
+        SpringApplication.run(TeamEmployeesApplication.class, args);
+    }
 
 }
--- src/test/java/com/exam/sirma/teamemployees/TeamEmployeesApplicationTests.java
+++ src/test/java/com/exam/sirma/teamemployees/TeamEmployeesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TeamEmployeesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
