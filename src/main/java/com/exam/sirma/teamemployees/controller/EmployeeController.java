3982aae5ab2b6b9c1aabe18934710c2019d299db
GorgiBg <georgid@orbisds.com>
2023-12-17 12:49:01 +0200

    add crud methods in EmployeeController

--- src/main/java/com/exam/sirma/teamemployees/controller/EmployeeController.java
+++ src/main/java/com/exam/sirma/teamemployees/controller/EmployeeController.java
@@ -1,7 +1,49 @@
 package com.exam.sirma.teamemployees.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.exam.sirma.teamemployees.entity.Employee;
+import com.exam.sirma.teamemployees.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        try {
+            Employee employee = employeeService.getEmployeeById(id);
+            return new ResponseEntity<>(employee, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
