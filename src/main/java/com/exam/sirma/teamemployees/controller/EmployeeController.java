dd2d0c2309be118e82735bd09669dd370646e821
GorgiBg <georgid@orbisds.com>
2023-12-17 12:38:36 +0200

    add crud methods in service and add empty EmployeeController

---
+++ src/main/java/com/exam/sirma/teamemployees/controller/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.exam.sirma.teamemployees.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
--- src/main/java/com/exam/sirma/teamemployees/service/EmployeeService.java
+++ src/main/java/com/exam/sirma/teamemployees/service/EmployeeService.java
@@ -5,10 +5,11 @@ import com.exam.sirma.teamemployees.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class EmployeeService {
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
 
     public EmployeeService(EmployeeRepository employeeRepository) {
@@ -17,5 +18,28 @@ public class EmployeeService {
 
     public void saveAllEmployees(List<Employee> employees) {
         employeeRepository.saveAll(employees);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        try {
+            Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("No Employee with ID: " + id + " found!"));
+            return employee;
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
     }
 }
